09fae8e611ca96492bd7c872e96c4af1ad13b578
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-12-04 22:24:15 +0000

    Update models.py

--- app/models.py
+++ app/models.py
@@ -8,8 +8,9 @@ class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model_name = models.CharField(max_length=50)
     available_from = models.DateField()
+    price = models.PositiveSmallIntegerField(max_length=7)
     manufacturer = models.CharField(max_length=50)
-    description = models.TextField(max_length=255, null=True)
+    description = models.TextField(max_length=255)
 
     def __str__(self):
         return str(self.model_name)
